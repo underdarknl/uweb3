c63e142ffe33d47c9f9ab58dcdb504a2c4561f02
Stef Houten <stefvanhouten@gmail.com>
2020-04-16 11:28:14 +0200

    Bugged download

--- uweb3/scaffold/base/pages.py
+++ uweb3/scaffold/base/pages.py
@@ -4,12 +4,19 @@
 import uweb3
 from uweb3.model import SettingsManager
 
+from uweb3 import response
+import os
+
 class PageMaker(uweb3.DebuggingPageMaker):
   """Holds all the request handlers for the application"""
 
   def Index(self):
     """Returns the index template"""
-    return self.parser.Parse('index.html')
+    file = open('/home/stef/devel/uweb3/uweb3/scaffold/uweb3_template.zip', 'rb')
+    fsize = os.path.getsize('/home/stef/devel/uweb3/uweb3/scaffold/uweb3_template.zip')
+    res = response.Response(content=file, content_type="application/zip", headers={'Content-Disposition': 'attachment; filename=uweb3_template.zip', 'Content-Length': fsize})
+    return res
+    # return self.parser.Parse('index.html')
 
   def TestRoute(self):
     """Returns the index template"""
