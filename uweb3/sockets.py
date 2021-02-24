88770e375245f243957e94866847f18262beae86
Jan <janklopper@innerheight.com>
2021-02-24 12:28:39 +0100

    linint cleanups and some minor refactoring

--- uweb3/sockets.py
+++ uweb3/sockets.py
@@ -12,18 +12,20 @@ class SocketMiddleWare(socketio.WSGIApp):
   def __init__(self, socketio_server, uweb3_server, socketio_path='socket.io'):
     super(SocketMiddleWare, self).__init__(socketio_server,
                                            uweb3_server,
-                                           socketio_path=socketio_path
-                                           )
+                                           socketio_path=socketio_path)
 
 class Uweb3SocketIO:
-  def __init__(self, app, sio, static_dir=os.path.dirname(os.path.abspath(__file__))):
+  def __init__(self, app, sio, static_dir=None):
     if not isinstance(app, uWeb):
       raise Exception("App must be an uWeb3 instance!")
+    if not static_dir:
+      static_dir = os.path.dirname(os.path.abspath(__file__))
 
     self.host = app.config.options['development'].get('host', '127.0.0.1')
     self.port = app.config.options['development'].get('port', 8000)
     if app.config.options['development'].get('dev', False) == 'True':
-      HotReload(app.executing_path, uweb_dev=app.config.options['development'].get('uweb_dev', 'False'))
+      hotreload = app.config.options['development'].get('uweb_dev', 'False')
+      HotReload(app.executing_path, uweb_dev=hotreload)
     self.setup_app(app, sio, static_dir)
 
 
@@ -31,5 +33,6 @@ class Uweb3SocketIO:
     path = os.path.join(app.executing_path, 'static')
     app = SocketMiddleWare(sio, app)
     static_directory = [os.path.join(sys.path[0], path)]
-    app = StaticMiddleware(app, static_root='static', static_dirs=static_directory)
+    app = StaticMiddleware(app, static_root='static',
+                           static_dirs=static_directory)
     eventlet.wsgi.server(eventlet.listen((self.host, int(self.port))), app)
