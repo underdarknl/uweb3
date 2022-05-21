622f69ea0e4869d73878e917e5c65bd42f6517fb
stefvanhouten <stefvanhouten@gmail.com>
2022-05-21 16:17:03 +0200

    Update connection func

--- uweb3/libs/sqltalk/postgres/__init__.py
+++ uweb3/libs/sqltalk/postgres/__init__.py
@@ -3,8 +3,11 @@ __author__ = 'Stef van Houten <stef@underdark.nl>'
 __version__ = '0.1'
 
 # Application specific modules
+import psycopg2
+from . import cursor
 from . import connection
 
-def Connect(*args, **kwargs):
+def Connect(*args, **kwargs) -> connection.Connection:
   """Factory function for connection.Connection."""
-  return connection.Connection(*args, **kwargs)
+  return psycopg2.connect(connection_factory=connection.Connection, cursor_factory=cursor.Cursor, *args, **kwargs)  # type: ignore
+
