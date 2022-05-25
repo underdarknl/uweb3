2d1df06219612cd7e7494630473d50dc1fc81bc9
stefvanhouten <stefvanhouten@gmail.com>
2022-05-25 18:23:24 +0200

    Update connection.py

--- uweb3/libs/sqltalk/postgres/connection.py
+++ uweb3/libs/sqltalk/postgres/connection.py
@@ -110,13 +110,14 @@ class Connection(psycopg2.extensions.connection, BaseConnection):
             # 'transactioncount': self.counter_transactions
         }
 
-        Error = psycopg2.Error
-        InterfaceError = psycopg2.InterfaceError
-        DatabaseError = psycopg2.DatabaseError
-        DataError = psycopg2.DataError
-        OperationalError = psycopg2.OperationalError
-        IntegrityError = psycopg2.IntegrityError
-        InternalError = psycopg2.InternalError
-        ProgrammingError = psycopg2.ProgrammingError
-        NotSupportedError = psycopg2.NotSupportedError
-        Warning = psycopg2.Warning
+
+Error = psycopg2.Error
+InterfaceError = psycopg2.InterfaceError
+DatabaseError = psycopg2.DatabaseError
+DataError = psycopg2.DataError
+OperationalError = psycopg2.OperationalError
+IntegrityError = psycopg2.IntegrityError
+InternalError = psycopg2.InternalError
+ProgrammingError = psycopg2.ProgrammingError
+NotSupportedError = psycopg2.NotSupportedError
+Warning = psycopg2.Warning
