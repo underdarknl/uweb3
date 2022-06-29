3b839921428bbb27770f847e9b4e737607005799
stefvanhouten <stefvanhouten@gmail.com>
2022-06-29 15:59:57 +0200

    Add scaffold for sortable pagination

--- uweb3/pagination.py
+++ uweb3/pagination.py
@@ -1,3 +1,4 @@
+from operator import attrgetter, itemgetter
 from typing import Iterable, Union
 from uweb3.pagemaker import IndexedFieldStorage
 from uweb3.templateparser import Parser
@@ -46,13 +47,13 @@ class BasePagination:
     def __init__(
         self, data: Iterable, get_req_dict: Union[IndexedFieldStorage, None] = None
     ):
-        self._page_number = 0
+        self._page_number = 1
         self.get_data = get_req_dict
 
         self.pages = list(_chunkify(data, self.page_size))
-        self.total_pages = len(self.pages) - 1
+        self.total_pages = len(self.pages)
 
-        if self.get_data.getfirst("page"):
+        if self.get_data and self.get_data.getfirst("page"):
             self.page_number = self.get_data.getfirst("page")
 
     def get_page(self, page_number: int):
@@ -64,7 +65,7 @@ class BasePagination:
     def current_page(self):
         if not self.pages:
             raise NoPageError(f"No page for page number {self.page_number}")
-        return self.pages[self.page_number]
+        return self.pages[self.page_number - 1]
 
     @property
     def page_number(self):
@@ -87,53 +88,85 @@ class BasePagination:
 
     @property
     def next_page(self):
-        if self.page_number < self.total_pages - 1:
+        if self.page_number < self.total_pages:
             return self.page_number + 1
         else:
             return self.page_number
 
     @property
     def previous_page(self):
-        if self.page_number > 0:
+        if self.page_number > 1:
             return self.page_number - 1
         else:
             return self.page_number
 
     @property
     def render_nav(self):
-        nav_start = self.previous_page
-        nav_end = min(nav_start + 4, self.total_pages + 1)
-        ranges = range(nav_start, nav_end)
+        ranges = self._determine_page_numbers()
+        query_url = "".join(
+            [f"&{key}={self.get_data.getfirst(key)}" for key in self.relevant_keys()]
+        )
         return Parser().ParseString(
             """
             <nav class="pagination">
                 <ol>
-                    {{ if [__paginator:page_number] != 0 }}
-                        <li><a href="?page=first">First</a></li>
+                    {{ if [__paginator:page_number] != 1 }}
+                        <li><a href="?page=first[__query_url]">First</a></li>
                     {{ endif }}
                     {{ for p in [__ranges] }}
-                        <li><a href="?page=[p]">[p]</a></li>
+                        <li><a href="?page=[p][__query_url]">[p]</a></li>
                     {{ endfor }}
-                    <li><a href="?page=[__paginator:previous_page]">Previous</a></li>
+                    {{ if [__paginator:page_number] != 1 }}
+                        <li><a href="?page=[__paginator:previous_page][__query_url]">Previous</a></li>
+                    {{ endif }}
                     {{ if [__paginator:page_number] <  [__paginator:total_pages]  }}
-                        <li><a href="?page=[__paginator:next_page]">Next</a></li>
+                        <li><a href="?page=[__paginator:next_page][__query_url]">Next</a></li>
                     {{ endif }}
                     {{ if [__paginator:page_number] != [__paginator:total_pages] }}
-                        <li><a href="?page=last">Last</a></li>
+                        <li><a href="?page=last[__query_url]">Last</a></li>
                     {{ endif }}
                 </ol>
             </nav>
             """,
             __paginator=self,
             __ranges=ranges,
+            __query_url=query_url,
         )
+
+    def relevant_keys(self):
+        return []
+
+    def _determine_page_numbers(self):
+        nav_start = self.previous_page
+        nav_end = min(nav_start + 4, self.total_pages + 1)
+        if self.total_pages - self.page_number < 2:
+            return range(nav_start - 2, nav_end)
+        return range(nav_start, nav_end)
 
     def _determine_page(self, value: str):
         if value == "first":
-            return 0
+            return 1
         if value == "last":
             return self.total_pages
         raise InvalidPageNumber(f"Value {value} for page number is not supported")
 
     def __iter__(self):
         return iter(self.pages)
+
+
+class SortablePagination(BasePagination):
+    def __init__(
+        self, data: Iterable, get_req_dict: Union[IndexedFieldStorage, None] = None
+    ):
+        data = list(data)
+        order = get_req_dict.getfirst("order", "ASC")
+        if order == "ASC":
+            data.sort(key=itemgetter(get_req_dict.getfirst("sort")), reverse=False)
+        else:
+            data.sort(key=itemgetter(get_req_dict.getfirst("sort")), reverse=True)
+
+        super().__init__(data=data, get_req_dict=get_req_dict)
+
+    def relevant_keys(self):
+        keys = super().relevant_keys()
+        return keys + ["sort", "order"]
