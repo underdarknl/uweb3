0788dab663c40b3dd1091aca68032feb7657bf75
stefvanhouten <stefvanhouten@gmail.com>
2022-07-07 13:07:11 +0200

    Copy LimitedStream from werkzeug

--- uweb3/request.py
+++ uweb3/request.py
@@ -78,46 +78,162 @@ class Cookie(cookie.SimpleCookie):
             dict.__setitem__(self, key, morsel)
 
 
-class BaseRequest:
-    MAX_COOKIE_LENGTH: int = 4 * 1024  # 4KB
-    MAX_REQUEST_BODY_SIZE: int = 20 * 1024 * 1024  # 20MB
+# This code is copied from the Werkzeug repository.
+# https://github.com/pallets/werkzeug/blob/main/LICENSE.rst
+# https://github.com/pallets/werkzeug/blob/d36aaf12b5d12634844e4c7f5dab4a8282688e12/src/werkzeug/wsgi.py#L828
+class LimitedStream(io.IOBase):
+    """Wraps a stream so that it doesn't read more than n bytes.  If the
+    stream is exhausted and the caller tries to get more bytes from it
+    :func:`on_exhausted` is called which by default returns an empty
+    string.  The return value of that function is forwarded
+    to the reader function.  So if it returns an empty string
+    :meth:`read` will return an empty string as well.
 
-    def __init__(self, env):
-        self.env = env
-        self.charset = "utf-8"
-        self.headers = dict(headers_from_env(env))
-        self.noparse = self.headers.get("accept", "").lower() == "application/json"
+    The limit however must never be higher than what the stream can
+    output.  Otherwise :meth:`readlines` will try to read past the
+    limit.
 
-        self.env["host"] = self.headers.get("Host", "").strip().lower()
-        self.env["REAL_REMOTE_ADDR"] = self._return_real_remote_addr()
-        self.env["mimetype"] = self._get_mimetype()
+    .. admonition:: Note on WSGI compliance
 
-        self.method = self.env["REQUEST_METHOD"]
-        self.path = self.env["PATH_INFO"]
+       calls to :meth:`readline` and :meth:`readlines` are not
+       WSGI compliant because it passes a size argument to the
+       readline methods.  Unfortunately the WSGI PEP is not safely
+       implementable without a size argument to :meth:`readline`
+       because there is no EOF marker in the stream.  As a result
+       of that the use of :meth:`readline` is discouraged.
 
-        self.vars = {
-            "cookie": {
-                name: value.value
-                for name, value in Cookie(self.env.get("HTTP_COOKIE")).items()
-            },
-            "get": QueryArgsDict(parse_qs(self.env["QUERY_STRING"])),
-            "post": IndexedFieldStorage(),
-            "put": IndexedFieldStorage(),
-            "delete": IndexedFieldStorage(),
-            "files": IndexedFieldStorage(),
-        }
+       For the same reason iterating over the :class:`LimitedStream`
+       is not portable.  It internally calls :meth:`readline`.
 
-    def _get_mimetype(self):
-        return self.env.get("CONTENT_TYPE", "").split(";")[0]
+       We strongly suggest using :meth:`read` only or using the
+       :func:`make_line_iter` which safely iterates line-based
+       over a WSGI input stream.
 
-    def _return_real_remote_addr(self):
-        """Returns the remote ip-address,
-        if there is a proxy involved it will take the last IP addres from the HTTP_X_FORWARDED_FOR list
+    :param stream: the stream to wrap.
+    :param limit: the limit for the stream, must not be longer than
+                  what the string can provide if the stream does not
+                  end with `EOF` (like `wsgi.input`)
+    """
+
+    def __init__(self, stream, limit: int) -> None:
+        self._read = stream.read
+        self._readline = stream.readline
+        self._pos = 0
+        self.limit = limit
+
+    def __iter__(self) -> "LimitedStream":
+        return self
+
+    @property
+    def is_exhausted(self) -> bool:
+        """If the stream is exhausted this attribute is `True`."""
+        return self._pos >= self.limit
+
+    def on_exhausted(self) -> bytes:
+        """This is called when the stream tries to read past the limit.
+        The return value of this function is returned from the reading
+        function.
         """
+        # Read null bytes from the stream so that we get the
+        # correct end of stream marker.
+        return self._read(0)
+
+    def on_disconnect(self):
+        """What should happen if a disconnect is detected?  The return
+        value of this function is returned from read functions in case
+        the client went away.  By default a
+        :exc:`~werkzeug.exceptions.ClientDisconnected` exception is raised.
+        """
+        raise ClientDisconnected()
+
+    def exhaust(self, chunk_size: int = 1024 * 64) -> None:
+        """Exhaust the stream.  This consumes all the data left until the
+        limit is reached.
+
+        :param chunk_size: the size for a chunk.  It will read the chunk
+                           until the stream is exhausted and throw away
+                           the results.
+        """
+        to_read = self.limit - self._pos
+        chunk = chunk_size
+        while to_read > 0:
+            chunk = min(to_read, chunk)
+            self.read(chunk)
+            to_read -= chunk
+
+    def read(self, size=None) -> bytes:
+        """Read `size` bytes or if size is not provided everything is read.
+
+        :param size: the number of bytes read.
+        """
+        if self._pos >= self.limit:
+            return self.on_exhausted()
+        if size is None or size == -1:  # -1 is for consistence with file
+            size = self.limit
+        to_read = min(self.limit - self._pos, size)
         try:
-            return self.env["HTTP_X_FORWARDED_FOR"].split(",")[-1].strip()
-        except KeyError:
-            return self.env["REMOTE_ADDR"]
+            read = self._read(to_read)
+        except (OSError, ValueError):
+            return self.on_disconnect()
+        if to_read and len(read) != to_read:
+            return self.on_disconnect()
+        self._pos += len(read)
+        return read
+
+    def readline(self, size=None) -> bytes:
+        """Reads one line from the stream."""
+        if self._pos >= self.limit:
+            return self.on_exhausted()
+        if size is None:
+            size = self.limit - self._pos
+        else:
+            size = min(size, self.limit - self._pos)
+        try:
+            line = self._readline(size)
+        except (ValueError, OSError):
+            return self.on_disconnect()
+        if size and not line:
+            return self.on_disconnect()
+        self._pos += len(line)
+        return line
+
+    def readlines(self, size=None):
+        """Reads a file into a list of strings.  It calls :meth:`readline`
+        until the file is read to the end.  It does support the optional
+        `size` argument if the underlying stream supports it for
+        `readline`.
+        """
+        last_pos = self._pos
+        result = []
+        if size is not None:
+            end = min(self.limit, last_pos + size)
+        else:
+            end = self.limit
+        while True:
+            if size is not None:
+                size -= last_pos - self._pos
+            if self._pos >= end:
+                break
+            result.append(self.readline(size))
+            if size is not None:
+                last_pos = self._pos
+        return result
+
+    def tell(self) -> int:
+        """Returns the position of the stream.
+
+        .. versionadded:: 0.9
+        """
+        return self._pos
+
+    def __next__(self) -> bytes:
+        line = self.readline()
+        if not line:
+            raise StopIteration()
+        return line
+
+    def readable(self) -> bool:
+        return True
 
 
 class DataParser:
